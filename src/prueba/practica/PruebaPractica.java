6fb11a2e1235c11add6619319d0a2275f60ddb62
Denise Barrientos <gisellebarrientos22@gmail.com>
2024-01-17 22:28:18 +0100

    mady solo tiene que corregir lo que quedó

--- src/prueba/practica/PruebaPractica.java
+++ src/prueba/practica/PruebaPractica.java
@@ -14,6 +14,19 @@ public class PruebaPractica {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+         int num1=10;
+        int num2=5;
+ 
+        /*Realizamos las operaciones.
+         * Tambien lo podemos guardar el resultado en variables. */
+ 
+        System.out.println("El resultado de la resta es "+(num1+num2))
+ 
+        System.out.println("El resultado de la resta es "+(num1-num2));
+ 
+        System.out.println("El resultado de la division es "+(num1*num2);
+ 
+        System.out.println("El resultado de la división es "+(num1/num2));
         // TODO code application logic here
     }
     
